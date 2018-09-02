d53523bcbc57b067efbb3aeebd505a197e130a57
Rafael Silva <rafaelsilvarnj@gmail.com>
2018-09-02 14:43:41 -0300

    Commit inicial - Configurado o arquivo pom.xml para incluir o primefaces; - Configurado o java faces no web.xml; - Criado bean inicial.

---
+++ README.md
@@ -0,0 +1,1 @@
+Projeto de desenvolvimento de um site que recebe arquivo de vídeo não exibível, o altera e exibe para o usuário em formato reconhecível.
---
+++ src/main/java/com/test/samba/bean/UploadBean.java
@@ -0,0 +1,14 @@
+package com.test.samba.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class UploadBean {
+
+	public UploadBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
