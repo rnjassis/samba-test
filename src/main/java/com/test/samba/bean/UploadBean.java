0fb1bb95b36eef12c16d288cc1b03f059dc5f173
Rafael Silva <rafaelsilvarnj@gmail.com>
2018-09-03 13:27:54 -0300

    Criado página de upload

--- src/main/java/com/test/samba/bean/UploadBean.java
+++ src/main/java/com/test/samba/bean/UploadBean.java
@@ -3,12 +3,20 @@ package com.test.samba.bean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
 @ManagedBean
 @SessionScoped
 public class UploadBean {
-
+	
 	public UploadBean() {
 		// TODO Auto-generated constructor stub
 	}
 
+	public void handleFileUpload(FileUploadEvent event) {
+		UploadedFile file = event.getFile();
+		
+		System.out.println("handled");
+	}
 }
